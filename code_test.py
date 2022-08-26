0974f54f7968254344f204b5d3cd0538973e4c49
Inderway <47097657+Inderway@users.noreply.github.com>
2022-08-26 17:08:24 +0800

    u

--- code_test.py
+++ code_test.py
@@ -1,6 +1,11 @@
 import torch
 import os
+from torch.nn.utils.rnn import pad_sequence
 
-print(os.getcwd())
-
+a=torch.tensor([0,1,2,3,4,10])
+b=torch.tensor([0,4,5,6,7,8,10])
+c=torch.tensor([0,7,8,9,9,11,12,10])
+de_batch=[a,b,c]
+de_batch = pad_sequence(de_batch, padding_value=99)
+print(de_batch)
 
